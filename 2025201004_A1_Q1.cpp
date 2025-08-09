edd13bfe70c71b50c9d1d9f378a50d291bce76ac
Souradeep <dsouradeep.117@gmail.com>
2025-08-09 17:54:54 +0530

    Added implementation for Flag 1

--- 2025201004_A1_Q1.cpp
+++ 2025201004_A1_Q1.cpp
@@ -50,43 +50,63 @@ void printUsage() {
 }
 
 int main(int argc, char* argv[]) {
-    // Argument parsing
-    if (argc < 3) { printUsage(); _exit(1); }
+    if (argc < 3) {
+        printUsage();
+        _exit(1);
+    }
 
     const char* inputFile = argv[1];
     int mode = convertToInt(argv[2]);
 
+    // Validate mode & args
     if (mode == 0) {
         if (argc != 4) {
             fdWriteStr(2, "Error: Flag 0 requires block size.\n");
             printUsage();
             _exit(1);
         }
+    } else if (mode == 1) {
+        if (argc != 3) {
+            fdWriteStr(2, "Error: Flag 1 takes no extra arguments.\n");
+            printUsage();
+            _exit(1);
+        }
     } else {
-        fdWriteStr(2, "Only flag 0 implemented in this version.\n");
+        fdWriteStr(2, "Error: Only flag 0 and 1 implemented currently.\n");
+        printUsage();
         _exit(1);
     }
 
-    int blockSize = convertToInt(argv[3]);
-    if (blockSize <= 0) {
-        fdWriteStr(2, "Invalid block size.\n");
-        _exit(1);
-    }
-    if (blockSize > 8 * 1024 * 1024) {
-        fdWriteStr(2, "Warning: block size > 8MB.\n");
+    // For flag 0 parse block size
+    int blockSize = 0;
+    if (mode == 0) {
+        blockSize = convertToInt(argv[3]);
+        if (blockSize <= 0) {
+            fdWriteStr(2, "Invalid block size.\n");
+            _exit(1);
+        }
+        if (blockSize > 8 * 1024 * 1024) {
+            fdWriteStr(2, "Warning: block size > 8MB may be inefficient.\n");
+        }
+    } else {
+        // For flag 1 fixed chunk size
+        blockSize = 1024 * 1024; // 1MB chunks for efficiency
     }
 
     // Create Assignment1 directory
     if (mkdir("Assignment1", 0700) == -1 && errno != EEXIST) {
-        fdWriteStr(2, "mkdir Assignment1 failed\n");
+        fdWriteStr(2, "Failed to create Assignment1 directory.\n");
         _exit(1);
     }
 
     // Open input file
     int fd_in = open(inputFile, O_RDONLY);
-    if (fd_in == -1) { fdWriteStr(2, "Open input failed\n"); _exit(1); }
+    if (fd_in == -1) {
+        fdWriteStr(2, "Failed to open input file.\n");
+        _exit(1);
+    }
 
-    // Build dynamic output path: Assignment1/<flag>_<basename>
+    // Build output filename: Assignment1/<mode>_<basename>
     const char* baseName = inputFile;
     for (int i = 0; inputFile[i] != '\0'; i++) {
         if (inputFile[i] == '/') baseName = inputFile + i + 1;
@@ -100,60 +120,154 @@ int main(int argc, char* argv[]) {
     for (int i = 0; baseName[i] != '\0'; i++) outputPath[pos++] = baseName[i];
     outputPath[pos] = '\0';
 
-    // Create output file
+    // Open output file
     int fd_out = open(outputPath, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd_out == -1) { fdWriteStr(2, "Open output failed\n"); close(fd_in); _exit(1); }
-
-    // File size
-    off_t fileSize = lseek(fd_in, 0, SEEK_END);
-    if (fileSize < 0) { fdWriteStr(2, "lseek fail\n"); close(fd_in); close(fd_out); _exit(1); }
-    lseek(fd_in, 0, SEEK_SET);
-
-    // Allocate buffer with mmap
-    char* buffer = (char*) mmap(NULL, blockSize, PROT_READ | PROT_WRITE,
-                                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (buffer == MAP_FAILED) {
-        fdWriteStr(2, "Buffer alloc fail\n");
-        close(fd_in); close(fd_out);
+    if (fd_out == -1) {
+        fdWriteStr(2, "Failed to open output file.\n");
+        close(fd_in);
         _exit(1);
     }
 
-    off_t totalBlocks = (fileSize + blockSize - 1) / blockSize;
-    off_t doneBlocks = 0;
+    // Get file size
+    off_t fileSize = lseek(fd_in, 0, SEEK_END);
+    if (fileSize == (off_t)-1) {
+        fdWriteStr(2, "Failed to determine input file size.\n");
+        close(fd_in);
+        close(fd_out);
+        _exit(1);
+    }
 
-    while (doneBlocks < totalBlocks) {
-        ssize_t chunk = blockSize;
-        off_t left = fileSize - doneBlocks * blockSize;
-        if (left < blockSize) chunk = left;
+    // Reset input file pointer
+    if (lseek(fd_in, 0, SEEK_SET) == (off_t)-1) {
+        fdWriteStr(2, "Failed to reset input file pointer.\n");
+        close(fd_in);
+        close(fd_out);
+        _exit(1);
+    }
 
-        ssize_t rbytes = 0;
-        while (rbytes < chunk) {
-            ssize_t r = read(fd_in, buffer + rbytes, chunk - rbytes);
-            if (r <= 0) break;
-            rbytes += r;
+    // Allocate buffer
+    char* buffer = (char*) mmap(NULL, blockSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (buffer == MAP_FAILED) {
+        fdWriteStr(2, "Failed to allocate buffer memory.\n");
+        close(fd_in);
+        close(fd_out);
+        _exit(1);
+    }
+
+    if (mode == 0) {
+        // ----- Block-wise reversal (flag 0) -----
+        off_t totalBlocks = (fileSize + blockSize - 1) / blockSize;
+        off_t processedBlocks = 0;
+
+        while (processedBlocks < totalBlocks) {
+            ssize_t currentBlockSize = blockSize;
+            off_t bytesRemaining = fileSize - processedBlocks * blockSize;
+            if (bytesRemaining < blockSize) currentBlockSize = bytesRemaining;
+
+            ssize_t bytesRead = 0;
+            while (bytesRead < currentBlockSize) {
+                ssize_t r = read(fd_in, buffer + bytesRead, currentBlockSize - bytesRead);
+                if (r == -1) {
+                    fdWriteStr(2, "Error reading input file.\n");
+                    munmap(buffer, blockSize);
+                    close(fd_in);
+                    close(fd_out);
+                    _exit(1);
+                }
+                if (r == 0) break; // EOF
+                bytesRead += r;
+            }
+
+            if (bytesRead == 0) break;
+
+            // Reverse this block in place
+            for (ssize_t i = 0; i < bytesRead / 2; i++) {
+                char tmp = buffer[i];
+                buffer[i] = buffer[bytesRead - i - 1];
+                buffer[bytesRead - i - 1] = tmp;
+            }
+
+            ssize_t bytesWritten = 0;
+            while (bytesWritten < bytesRead) {
+                ssize_t w = write(fd_out, buffer + bytesWritten, bytesRead - bytesWritten);
+                if (w == -1) {
+                    fdWriteStr(2, "Error writing output file.\n");
+                    munmap(buffer, blockSize);
+                    close(fd_in);
+                    close(fd_out);
+                    _exit(1);
+                }
+                bytesWritten += w;
+            }
+
+            processedBlocks++;
+
+            // Show progress
+            write(1, "\rProgress: ", 11);
+            fdWriteInt(1, (int)((processedBlocks * 100) / totalBlocks));
+            write(1, "%", 1);
         }
-        if (rbytes == 0) break;
+    } else if (mode == 1) {
+        // ----- Full file reversal (flag 1) -----
+        off_t bytesProcessed = 0;
+        off_t remaining = fileSize;
 
-        // Reverse in place
-        for (ssize_t i = 0; i < rbytes / 2; i++) {
-            char t = buffer[i];
-            buffer[i] = buffer[rbytes - i - 1];
-            buffer[rbytes - i - 1] = t;
+        while (remaining > 0) {
+            ssize_t toRead = (remaining >= blockSize) ? blockSize : remaining;
+
+            // Move file pointer to position before this chunk
+            if (lseek(fd_in, remaining - toRead, SEEK_SET) == (off_t)-1) {
+                fdWriteStr(2, "Seek error in input file.\n");
+                munmap(buffer, blockSize);
+                close(fd_in);
+                close(fd_out);
+                _exit(1);
+            }
+
+            ssize_t bytesRead = 0;
+            while (bytesRead < toRead) {
+                ssize_t r = read(fd_in, buffer + bytesRead, toRead - bytesRead);
+                if (r < 0) {
+                    fdWriteStr(2, "Read error.\n");
+                    munmap(buffer, blockSize);
+                    close(fd_in);
+                    close(fd_out);
+                    _exit(1);
+                }
+                if (r == 0) break;
+                bytesRead += r;
+            }
+
+            if (bytesRead == 0) break;
+
+            // Reverse chunk bytes
+            for (ssize_t i = 0; i < bytesRead / 2; i++) {
+                char tmp = buffer[i];
+                buffer[i] = buffer[bytesRead - i - 1];
+                buffer[bytesRead - i - 1] = tmp;
+            }
+
+            ssize_t bytesWritten = 0;
+            while (bytesWritten < bytesRead) {
+                ssize_t w = write(fd_out, buffer + bytesWritten, bytesRead - bytesWritten);
+                if (w == -1) {
+                    fdWriteStr(2, "Write error.\n");
+                    munmap(buffer, blockSize);
+                    close(fd_in);
+                    close(fd_out);
+                    _exit(1);
+                }
+                bytesWritten += w;
+            }
+
+            bytesProcessed += bytesRead;
+            remaining -= bytesRead;
+
+            // Show progress
+            write(1, "\rProgress: ", 11);
+            fdWriteInt(1, (int)((bytesProcessed * 100) / fileSize));
+            write(1, "%", 1);
         }
-
-        ssize_t wbytes = 0;
-        while (wbytes < rbytes) {
-            ssize_t w = write(fd_out, buffer + wbytes, rbytes - wbytes);
-            if (w <= 0) break;
-            wbytes += w;
-        }
-
-        doneBlocks++;
-
-        // Progress output
-        write(1, "\rProgress: ", 11);
-        fdWriteInt(1, (int)((doneBlocks * 100) / totalBlocks));
-        write(1, "%", 1);
     }
 
     write(1, "\n", 1);
